a8207606d8dc24afc3f9ceb7ac43eec2325faa1b
dasipdnaodasd <63977534+dasipdnaodasd@users.noreply.github.com>
2020-08-18 10:38:44 +0100

    bot

--- bot.py
+++ bot.py
@@ -3,10 +3,14 @@ import os
 
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = ':')
+client = commands.Bot(command_prefix = '+')
 
 @client.event
 async def on_ready():
     print('On')
 
-client.run(os.environ['token'])
+@client.command()
+async def ping(ctx):
+    await ctx.send('hello')
+
+client.run('NzQ1MjExMTA3NjIyMjU2Njkw.Xzud7Q.DLt0UfTFXTPXgAwSOUKvh4XLS4Q')
