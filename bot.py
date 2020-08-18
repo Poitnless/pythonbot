48d9856dd496c2135c0daf90f4faf2e73389893a
dasipdnaodasd <63977534+dasipdnaodasd@users.noreply.github.com>
2020-08-18 11:24:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,32 +19,7 @@ async def +help(ctx):
     
 @client.command()
 async def +code(ctx):
-    await ctx.send('import discord
-import os
-
-from discord.ext import commands
-
-client = commands.Bot(command_prefix = '+')
-
-@client.event
-async def on_ready():
-
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('+help For Commands'))
-    
-    print('On')
-
-@client.command()
-async def +help(ctx):
-    await ctx.send('Type +code For Persistants PythonBot Code')
-
-    
-@client.command()
-async def +code(ctx):
-    await ctx.send('')
-
-
-client.run(os.environ['token'])
-')
+    await ctx.send('https://github.com/Poitnless/pythonbot')
 
 
 client.run(os.environ['token'])
