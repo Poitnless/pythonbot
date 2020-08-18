ad291479db6aa0ca443d946c814dacf9013eab72
dasipdnaodasd <63977534+dasipdnaodasd@users.noreply.github.com>
2020-08-18 11:18:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ client = commands.Bot(command_prefix = '+')
 @client.event
 async def on_ready():
 
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Type +help For Commands')
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Type +help For Commands'))
     
     print('On')
 
