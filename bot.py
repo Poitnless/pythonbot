1427e6fe84267418a7032849b0ce90d1ad33850e
dasipdnaodasd <63977534+dasipdnaodasd@users.noreply.github.com>
2020-08-18 11:56:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ client = commands.Bot(command_prefix = '+')
 @client.event
 async def on_ready():
 
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Type +code For Persistants PythonBot Code'))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('+code PythonBot Code'))
     
     print('On')
 
