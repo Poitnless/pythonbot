de450ee16f95045609e051a268c0ecbd6373eeee
dasipdnaodasd <63977534+dasipdnaodasd@users.noreply.github.com>
2020-08-18 11:51:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,6 @@ async def on_ready():
     
     print('On')
 
-@client.command()
-async def help(ctx):
-    await ctx.send('Type +code For Persistants PythonBot Code')
 
 
 client.run(os.environ['token'])
